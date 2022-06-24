56ff2e927a88dd6930c97aa2fa1c2e5c8033da55
Ajay KJ <102035919+Ajaykj684@users.noreply.github.com>
2022-06-24 18:33:39 +0530

    Update settings.py

--- bestbuyproject/settings.py
+++ bestbuyproject/settings.py
@@ -90,13 +90,8 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME'  : 'mydatabase',
-         'USER'  : 'postgres',
-         'PASSWORD' : '12345',
-         'HOST' : 'localhost',
-         'PORT' : '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
